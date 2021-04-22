4c7eead42d9c54d42284ebb27c4349ed3539b5ea
Ben Stetzer <benstetzer@gmail.com>
2021-04-22 17:47:50 -0400

    starting user signup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth.js');
 const { restoreUser } = require('../../utils/auth.js');
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.get('/set-token-cookie', asyncHandler(async (req, res) => {
   const user = await User.findOne({
