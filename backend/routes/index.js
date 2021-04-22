184dff67bc76de8b9393a09c922a9ea0ef04489c
Ben Stetzer <benstetzer@gmail.com>
2021-04-22 13:31:28 -0400

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+module.exports = router;
